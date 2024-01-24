a33e69dacc8273aa60b69fd27d5a03fb59f75f0a
Tepluhin Daniil <daniiltepluhin@rambler.ru>
2024-01-24 14:57:48 +0500

    first-iteration

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { createUser } = require('../controllers/users');
+const { urlChecking } = require('../utils/urlChecking');
 
 router.post('/', createUser);
 
 router.post('/', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlChecking),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }).unknown(true),
